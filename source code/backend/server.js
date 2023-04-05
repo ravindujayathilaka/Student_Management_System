417c7b9e8fa1563f407213859a8296ed65750048
rAVINDUpRABASHWARA <00prabashwara123@gmail.com>
2023-04-05 20:52:13 +0530

    202305042052

---
+++ source code/backend/server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyparser = require('body-parser');
---
+++ source code/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="title">
+      <h1 className="title">
+        Student Management System
+      </h1>
+    </div>
+  );
+}
+
+export default App;
